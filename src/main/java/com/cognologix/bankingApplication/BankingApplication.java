285dfd28168bbe9e5bd26b6b0c63d9bde33d2c8f
NiketanYadavCognologix <niketan.yadav@cognologix.com>
2022-12-07 15:39:42 +0530

    adding sample file

--- src/main/java/com/cognologix/bankingApplication/BankingApplication.java
+++ src/main/java/com/cognologix/bankingApplication/BankingApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication//(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class BankingApplication {
 
